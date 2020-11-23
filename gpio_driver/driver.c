416ca4c88c1504b8c7b2301c50ff02501b738b08
togai-r <togai-r@klab.com>
2020-11-23 17:58:29 +0900

    fix: add comment

--- gpio_driver/driver.c
+++ gpio_driver/driver.c
@@ -24,12 +24,12 @@ static struct cdev hoge_cdev; // キャラクタデバイス
 static struct class *hoge_class = NULL; // udevが認識するためのデバイスクラス
 
 // raspberry piのIO関連
-#define REG_ADDR_BASE 0x3F000000       /* bcm_host_get_peripheral_address()の方がbetter */
-#define REG_ADDR_GPIO_BASE   (REG_ADDR_BASE + 0x00200000)
-#define REG_ADDR_GPIO_GPFSEL_0     0x0000
+#define REG_ADDR_BASE 0x3F000000
+#define REG_ADDR_GPIO_BASE (REG_ADDR_BASE + 0x00200000)
+#define REG_ADDR_GPIO_GPFSEL_0 0x0000
 #define REG_ADDR_GPIO_OUTPUT_SET_0 0x001C
 #define REG_ADDR_GPIO_OUTPUT_CLR_0 0x0028
-#define REG_ADDR_GPIO_LEVEL_0      0x0034
+#define REG_ADDR_GPIO_LEVEL_0 0x0034
 
 
 static void set_register(unsigned int addr, unsigned int val)
